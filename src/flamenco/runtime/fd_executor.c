8704dbda6b7f8266a40d44637a8af5b184c11cd4
Josh Siegel <jsiegel@jumptrading.com>
2024-04-22 14:51:41 +0000

    runtime_flamenco: bank hash fixes

--- src/flamenco/runtime/fd_executor.c
+++ src/flamenco/runtime/fd_executor.c
@@ -758,12 +758,15 @@ fd_execute_txn_prepare_phase4( fd_exec_slot_ctx_t * slot_ctx,
     TODO this should probably not run on executable accounts
         Also iterate over LUT accounts */
   if( FD_FEATURE_ACTIVE( slot_ctx, set_exempt_rent_epoch_max ) ) {
-    fd_pubkey_t * tx_accs   = (fd_pubkey_t *)((uchar *)txn_ctx->_txn_raw->raw + txn_ctx->txn_descriptor->acct_addr_off);
-    for( ulong i = fd_txn_acct_iter_init( txn_ctx->txn_descriptor, FD_TXN_ACCT_CAT_WRITABLE  );
-          i != fd_txn_acct_iter_end(); i=fd_txn_acct_iter_next( i ) ) {
-      if( (i == 0) || fd_pubkey_is_sysvar_id( &tx_accs[i] ) )
+    fd_acct_addr_t  const * acct = fd_txn_get_acct_addrs( txn_ctx->txn_descriptor, (uchar *)txn_ctx->_txn_raw->raw );
+
+   for( fd_txn_acct_iter_t i=fd_txn_acct_iter_init( txn_ctx->txn_descriptor, FD_TXN_ACCT_CAT_WRITABLE );
+         i!=fd_txn_acct_iter_end(); i=fd_txn_acct_iter_next( i ) ) {
+     fd_pubkey_t  const * a = (fd_pubkey_t *) &acct[ fd_txn_acct_iter_idx( i ) ];
+
+      if( (i == 0) || fd_pubkey_is_sysvar_id( a ) )
         continue;
-      fd_set_exempt_rent_epoch_max( txn_ctx, &tx_accs[i] );
+      fd_set_exempt_rent_epoch_max( txn_ctx, a );
     }
   }
 
